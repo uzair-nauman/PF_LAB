dd2c8c4526c32528221197cca67dc7568e7c3447
uzair-nauman <uzair.nauman124@gmail.com>
2026-09-14 21:42:04 +0500

    Delete Task6.cpp

--- Task6.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	printf("Welcome to Programming\n\n");
-	printf("Name:\tAli\n");
-	printf("Grade:\tA\n\n");
-	printf("File Path: C:\\Users\\Student\n\n");
-	printf("He said: \"Hello\"\n");
-	return 0;
-}
